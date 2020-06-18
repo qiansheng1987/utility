622c4ec54d65450faba273b4cf5802b2baffeb55
qiansheng <qiansheng@zuche.com>
2020-06-18 15:21:40 +0800

    feat(Java AQS): AQS demo

---
+++ javaaqs/src/main/java/com/qiansheng/javaaqs/JavaaqsApplication.java
@@ -0,0 +1,13 @@
+package com.qiansheng.javaaqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaaqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaaqsApplication.class, args);
+	}
+
+}
---
+++ javaaqs/src/test/java/com/qiansheng/javaaqs/JavaaqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiansheng.javaaqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaaqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
